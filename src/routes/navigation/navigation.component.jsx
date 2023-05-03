494bc8c9d3045b67701fdef169d3a6dab2b7ff19
Britta <brittan.villegas@gmail.com>
2023-05-03 15:29:55 +0200

    added routing

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Link, Outlet } from 'react-router-dom';
+import { ReactComponent as StoreLogo } from '../../assets/logoipsum-289.svg';
+
+import './navigation.styles.scss';
+
+const Navigation = () => {
+    return (
+        <>
+            <div className='navigation'>
+                <Link className='logo-container' to='/'>
+                    <StoreLogo className='logo' />
+                </Link>
+                <div className='nav-links-container'>
+                    <Link className='nav-link' to='/shop'>
+                        Shop
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </>
+    )
+}
+
+export default Navigation;
