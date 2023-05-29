94474bd441795a6be02969486c485044758223c9
Britta <brittan.villegas@gmail.com>
2023-05-29 14:56:28 +0200

    set up auth, sign-in, db, and user collection creation

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyAVcdC-m_zWtn1y4Zpr13ktean9uO7Lqqo",
@@ -18,5 +19,30 @@ provider.setCustomParameters({
 });
 
 export const auth = getAuth();
-
 export const signInWithGooglePopUp = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    console.log(userDocRef);
+
+    const userSnapShot = await getDoc(userDocRef);
+    console.log(userSnapShot);
+    console.log(userSnapShot.exists());
+
+    if (!userSnapShot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (err) {
+            console.log('error creating the user', err.message)
+        }
+    }
+    return userDocRef;
+} 
